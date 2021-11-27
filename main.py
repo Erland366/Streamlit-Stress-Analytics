0908be7f77c8d6268c955b44c83a140828e7231f
Erland366 <erland.pg366@gmail.com>
2021-11-27 17:20:01 +0800

    Edit : main.py → Add Hello World

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import streamlit as st
 from multipage import save, MultiPage, start_app, clear_cache
-from src.page.hranalytics import hranalytics
-from src.page.hranalyticsgraph import hranalyticsgraph
-from src.page.model import model
+# from src.page.hranalytics import hranalytics
+# from src.page.hranalyticsgraph import hranalyticsgraph
+# from src.page.model import model
 from PIL import Image
 import os
 
-
-#Nyobain push kedua
-print("Hello world")
 def main():
-    st.set_page_config(page_title="HR Analytics", page_icon="📊", layout="wide",
+    st.set_page_config(page_title="Stress Analytics", page_icon="📊", layout="wide",
         initial_sidebar_state="expanded",
         menu_items={
             'Get Help': 'https://www.extremelycoolapp.com/help',
@@ -18,6 +15,7 @@ def main():
             'About': "# This is a header. This is an *extremely* cool app!"
         }
     )
+    st.text("Hello World")
 
     start_app()
 if __name__ == "__main__":
